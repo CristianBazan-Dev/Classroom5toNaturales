fdfb14e26aa29d96f6630e580457e54874e9afac
Johan <johanwebdesigner@gmail.com>
2022-10-02 16:51:01 -0300

    Img folder erased

--- js/cardText.js
+++ js/cardText.js
@@ -1,6 +1,6 @@
 const text1 = document.getElementById("card-text-4");
 
 text1.addEventListener('click',()=>{
-    document.location.href="./text/4.html"
+    document.location.href="./text/4.php"
 })
 
