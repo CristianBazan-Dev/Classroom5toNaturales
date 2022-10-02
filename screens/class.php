1433101e4423669a207f7655862bd213adb44389
Johan <johanwebdesigner@gmail.com>
2022-10-02 16:46:54 -0300

    Adding php format

---
+++ screens/class.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>5to. NaturaLengua y Literatura - Prof. Cristian</title>
+    <link rel="stylesheet" href="../css/style.css">
+    
+</head>
+<body>
+
+<div class="grid-container" id="content">
+    <header>
+       
+
+        <h1 class="title"><a href="../index.php">5to. CS NATURALES</a></h1>
+ 
+        <div id="student-name" class="student-name"></div>  
+        <nav class="nav">
+
+            <div id="student-name" class="student-name"></div>  
+                    <ul class ="ul-nav">
+                        <li id="student-name-userScreen" class="student-name-userScreen"></li>  
+                        <li class="li-nav" id="att">Atributo personal</li>
+                        <li class="li-nav"  id="missions">Misiones</li>
+                            <ul class="li-nav-missions">
+                                <li class="task task-I" id="task-I">La búsqueda de los recursos.</li>
+                                <li class="task task-II" id="task-II">Rescate de la reina.</li>
+                                <li class="task task-III" id="task-III">Encuentro con la magia.</li>
+                                <li class="task task-III" id="task-IV">Aprendiendo a leer mentes.</li>
+                            </ul> 
+                        <li class="li-nav" id="extraMissions">Misiones extra</li>
+                            <ul class="li-nav-missions">
+                                <li class="task extraTask-I" id="task-I">Resistente al dispositivo maléfico.</li>
+                                <li class="task extraTask-II" id="task-II">Participante activo en las asambleas del pueblo.</li>
+                                <li class="task extraTask-III" id="task-II">Inmune a los ataques de charlatanería.</li>
+                            </ul> 
+                        <li class="li-nav" id="level">Nivel</li>
+                        <li class="li-nav"><a href="" class="link-nav"></a></li>
+                    </ul>
+
+                
+
+                    <div class="closeIcon-user">
+                        <span class="bar-user-1"></span>
+                        <span class="bar-user-2"></span>
+                    </div>
+
+                     <div class="modalInfo-container">
+                        <div class="modalInfo"  id="modalInfo">
+                            <div class="modal"></div>
+                        </div>
+                        
+                        <button class="buttonModalInfo" id="buttonInfoOk">Ok</button>
+                     </div>
+                </nav>
+    </header>
+
+    <main>
+       <div class="img-container"> 
+        <h1 class="class-title">El rol de la mujer en el realismo mágico</h1>
+        <img class="presentation-img" src="../img/clases-1.jpg" alt="Isabel Allende y el realismo mágico">
+       
+        <div class="themes-class-container">
+            
+                <div class="card-class-container" id="card-text-1"> 
+                    <div class="card-class text-1" >
+                        <h1></h1>
+                        <p></p>
+                    </div>
+                
+                </div>
+            
+
+                <div class="card-class-container" id="card-text-2">
+                    <div class="card-class text-2">
+                        <h1>3</h1>
+                        <p ></p>
+                    </div>
+                </div>
+
+                
+            <div class="card-class-container" id="card-text-3">
+                <div class="card-class text-3">
+                    <h1></h1>
+                    <p></p>
+                </div>
+            </div>
+    
+            <div class="card-class-container" id="card-text-4">
+                <div class="card-class text-4">
+                    <h1>1.</h1>
+                    <p></p>
+                </div>
+            </div>
+
+        </div>
+    
+            
+        </div> 
+   
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</div>
+
+    
+    <script src="../js/cardText.js"></script>
+    <script src="../js/menu.js"></script>
+    <script src="../js/studentTitle.js"></script>
+    <script src="../js/missionsData.js"></script>
+</body>
+</html>
